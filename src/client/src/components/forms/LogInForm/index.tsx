35f5edd2b1f8b3a778ed2b40aaec463aece70cb9
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-02-17 22:27:55 -0400

    form style updates

--- src/client/src/components/forms/LogInForm/index.tsx
+++ src/client/src/components/forms/LogInForm/index.tsx
@@ -1,3 +1,5 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons"
+import { Button } from "antd"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import userActions from "../../../redux/actions/user"
@@ -37,28 +39,42 @@ export default function LogInForm() {
                 <input
                     name="username"
                     id="username"
+                    placeholder="Username"
                     type="text"
                     onChange={(e) => handleLoginInfoChange(e?.target?.value, 'username')}
                     className='mr-1'
                 >  
                 </input>
+                <LockOutlined/>
             </div>
             <div className="form-row">
                 <input
                     name="password"
                     id="password"
+                    placeholder="Password"
                     type="text"
                     onChange={(e) => handleLoginInfoChange(e?.target?.value, 'password')}
                     className='mr-1'
                 >
                 </input>
+                <UserOutlined/>
             </div>
-            <div className="flex jc-sb">
-                <button className="hcp" onClick={() => onSubmitLogin(loginInfo)}>
+            <div>
+                <Button className="hcp w-100" onClick={() => onSubmitLogin(loginInfo)}>
                     Submit
-                </button>
-                <div>
-                    <span className='forgot_password_text'>Forgot Password?</span>
+                </Button>
+                <div className="flex jc-sb w-100">
+                    <div className="sub-text">
+                        <span className='forgot_password_text'>Forgot Password?</span>
+                    </div>
+                    <div className="sub-text">
+                        <span 
+                            className='forgot_password_text'
+                            onClick={() => navigate('/signup')}    
+                        >
+                            Sign Up
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
