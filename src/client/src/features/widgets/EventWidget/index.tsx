8bdd5e195a2dca5ff82d96e474001ccee9a82cc2
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-04-11 20:01:20 -0400

    event form widget

--- src/client/src/features/widgets/EventWidget/index.tsx
+++ src/client/src/features/widgets/EventWidget/index.tsx
@@ -1,21 +1,60 @@
 import { CloseOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
-import React from 'react'
+import { Button, Tabs } from 'antd'
+import React, { useMemo, useState } from 'react'
 import Draggable from 'react-draggable'
 import widgetActions from '../../../redux/actions/widget'
 import { store } from '../../../redux/store'
 import './styles.css'
+import NewEventForm from '../../../components/forms/NewEventForm'
+import { useSelector } from 'react-redux'
 
 
 
 export default function EventWidget() {
 
+    const eventWidget = useSelector((state: any) => state.widgets?.eventWidget ?? [])
+    const [activeProject, setActiveProject] = useState<any>(false)
+    const [editorType, setEditorType] = useState<any>('add')
+
+
+    useMemo(() => {
+        if (eventWidget?.activeProject) {
+            setActiveProject(eventWidget?.activeProject)
+        }
+        if (eventWidget?.editorType) {
+            setEditorType(eventWidget?.editorType)
+        }
+    }, [eventWidget])
+
     const closeWidget = () => {
-        store.dispatch(widgetActions.hideTaskWidget())
+        store.dispatch(widgetActions.hideEventWidget())
     }
 
+    const items = [
+        {
+            label: 'Event Form',
+            key: 'Event Form',
+            children: (
+                <div>
+                    <NewEventForm 
+                        formOperation={editorType}
+                    />
+                </div>
+            )
+        },
+        {
+            label: 'Events',
+            key: 'Events',
+            children: (
+                <div>
+                    EventEventEventEventEventEventEvent
+                </div>
+            )
+        },
+    ]
+
     return (
-        <Draggable>
+        <Draggable handle='.event-widget-header'>
             <div className='event-widget'>
                 <div className='event-widget-header'>
                     <div className='flex'>
@@ -35,7 +74,7 @@ export default function EventWidget() {
                     </div>
                 </div>
                 <div className='event-widget-body'>
-                    Event Widget
+                    <Tabs items={items} />
                 </div>
             </div>
         </Draggable>
