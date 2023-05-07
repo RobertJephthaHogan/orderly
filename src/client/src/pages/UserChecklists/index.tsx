1622e34cbd3bc4cdcbe1853cedd2f8d974bb6a68
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-05-07 19:21:30 -0400

    checklist latest

--- src/client/src/pages/UserChecklists/index.tsx
+++ src/client/src/pages/UserChecklists/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './styles.css'
 
 
 
@@ -8,6 +8,14 @@ export default function UserChecklists() {
     return (
         <div>
             User Checklists
+            <div className='w-100 flex'>
+                <div className='w-30'>
+                    Checklist by category
+                </div>
+                <div className='w-70'>
+                    Checklist area
+                </div>
+            </div>
         </div>
     )
 }
