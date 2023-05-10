c60776fbf21e5cdec921c49dbdeef95b36050c20
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-05-10 10:55:49 -0400

    intake and checklist widget

--- src/client/src/features/widgets/ChecklistWidget/index.tsx
+++ src/client/src/features/widgets/ChecklistWidget/index.tsx
@@ -1,13 +1,64 @@
 import React from 'react'
-
+import { store } from '../../../redux/store'
+import widgetActions from '../../../redux/actions/widget'
+import Draggable from 'react-draggable'
+import { Button, Tabs } from 'antd'
+import { CloseOutlined } from '@ant-design/icons'
+import './styles.css'
 
 
 
 export default function ChecklistWidget() {
 
+    const closeWidget = () => {
+        store.dispatch(widgetActions.hideChecklistWidget())
+    }
+
+    const items = [
+        {
+            label: 'Checklist Form',
+            key: 'Checklist Form',
+            children: (
+                <div>
+                    Checklist Form Will be here ---------------
+                </div>
+            )
+        },
+        {
+            label: 'Checklist',
+            key: 'Checklist',
+            children: (
+                <div>
+                    ChecklistChecklistChecklistChecklistChecklistChecklist
+                </div>
+            )
+        },
+    ]
+
     return (
-        <div>
-            Checklist Widget
-        </div>
+        <Draggable>
+            <div className='checklist-widget'>
+                <div className='checklist-widget-header'>
+                    <div className='flex'>
+                        {/* <h5 className='m-0 p-0'>Task Widget</h5> */}
+                    </div>
+                    <div>
+                        <Button 
+                            type='text' 
+                            className='btn-close-checklist-widget'
+                            style={{height:"auto"}}
+                            onClick={() => closeWidget()}
+                        >
+                            <h6 className='p-0 m-0'>
+                                <CloseOutlined className='p-0 m-0'/>
+                            </h6>
+                        </Button>
+                    </div>
+                </div>
+                <div className='checklist-widget-body'>
+                    <Tabs items={items} />
+                </div>
+            </div>
+        </Draggable>
     )
 }
--- src/client/src/features/widgets/IntakeWidget/index.tsx
+++ src/client/src/features/widgets/IntakeWidget/index.tsx
@@ -1,13 +1,64 @@
 import React from 'react'
-
+import { store } from '../../../redux/store'
+import widgetActions from '../../../redux/actions/widget'
+import Draggable from 'react-draggable'
+import { Button, Tabs } from 'antd'
+import { CloseOutlined } from '@ant-design/icons'
+import './styles.css'
 
 
 
 export default function IntakeWidget() {
 
+    const closeWidget = () => {
+        store.dispatch(widgetActions.hideIntakeWidget())
+    }
+
+    const items = [
+        {
+            label: 'Intake Form',
+            key: 'Intake Form',
+            children: (
+                <div>
+                    Intake Form Will be here ----------
+                </div>
+            )
+        },
+        {
+            label: 'Intake',
+            key: 'Intake',
+            children: (
+                <div>
+                    IntakeIntakeIntakeIntakeIntakeIntakeIntakeIntakeIntake
+                </div>
+            )
+        },
+    ]
+
     return (
-        <div>
-            Intake Widget
-        </div>
+        <Draggable>
+            <div className='intake-widget'>
+                <div className='intake-widget-header'>
+                    <div className='flex'>
+                        {/* <h5 className='m-0 p-0'>Intake Widget</h5> */}
+                    </div>
+                    <div>
+                        <Button 
+                            type='text' 
+                            className='btn-close-intake-widget'
+                            style={{height:"auto"}}
+                            onClick={() => closeWidget()}
+                        >
+                            <h6 className='p-0 m-0'>
+                                <CloseOutlined className='p-0 m-0'/>
+                            </h6>
+                        </Button>
+                    </div>
+                </div>
+                <div className='intake-widget-body'>
+                    <Tabs items={items} />
+                </div>
+            </div>
+        </Draggable>
     )
 }
