03f08f23efb0a9c5b6b8e21a7686259ffdc51e02
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-05-23 21:09:25 -0400

    user nutrition

--- src/client/src/pages/UserNutrition/index.tsx
+++ src/client/src/pages/UserNutrition/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import './styles.css'
 
 
 
 export default function UserNutrition() {
 
     return (
-        <div>
+        <div className='user-nutrition-component p-1'>
             User Nutrition
+            <div className='user-nutrition-top-bar'>
+                topbar
+            </div>
+            <div className='user-nutrition-card'>
+                body 
+            </div>
         </div>
     )
 }
