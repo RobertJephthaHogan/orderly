b79ed7545adea69a035f4e408f1f46856b69d5ca
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-05-24 12:56:31 -0400

    user settings latest

--- src/client/src/pages/UserSettings/index.tsx
+++ src/client/src/pages/UserSettings/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './styles.css'
+import { useSelector } from 'react-redux'
+import { EditOutlined } from '@ant-design/icons'
 
 
 
 export default function UserSettings() {
+
+    const currentUser = useSelector((state: any) => state.user?.data ?? [])
+
+    console.log('currentUser', currentUser)
 
     return (
         <div className='user-settings-component p-1'>
@@ -12,55 +18,150 @@ export default function UserSettings() {
             </div>
             <div>
                 <div className='user-info-card  mb-1'>
-                    <div>
-                        User information card
+                    <div className='p-2'>
+                        <h4>User information card</h4>
                     </div>
                     <div className='divider' />
-                    <div>
-                        First Name
+                    <div className=' info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'>First Name : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> {currentUser?.firstName} </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Last Name
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'>Last Name : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> {currentUser?.lastName} </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Email
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Email : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> {currentUser?.email} </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Password
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Password : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> xxxxxxxxxxx </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Phone Number
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Phone Number : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> {currentUser?.phoneNumber} </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Height
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Height : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> 5' 10'' </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Weight
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Weight : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> 170 </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Body Fat Percentage
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Body Fat Percentage : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> 12 </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Target Weight
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Target Weight : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> 215 </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
-                    <div>
-                        Target BFP
+                    <div className='info-row flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Target BFP : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> 7 </h5>
+                        </div>
+                        <div className='pl-4 edit-icon'>
+                            <h5 className='vm-auto'><EditOutlined /> </h5>
+                        </div>
                     </div>
                     
                 </div>
                 <div className='user-info-card  mb-1'>
-                    <div>
-                        User nutrition settings card
+                    <div className='p-2'>
+                        <h4>User nutrition settings card</h4>
                     </div>
                     <div className='divider' />
-                    <div>
-                        water intake goals
+                    <div className='flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Water Intake Goals : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> 124 </h5>
+                        </div>
                     </div>
-                    <div>
-                        calorie goals
+                    <div className='flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Calorie Goals : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> Calorie Goals </h5>
+                        </div>
                     </div>
-                    <div>
-                        macro goals
+                    <div className='flex p-1 pl-3'>
+                        <div>
+                            <h5 className='vm-auto'> Macro Goals : </h5>
+                        </div>
+                        <div className='pl-2'> 
+                            <h5 className='vm-auto'> macro goals </h5>
+                        </div>
                     </div>
 
                 </div>
