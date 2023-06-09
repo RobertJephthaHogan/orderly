faddf1834b6bf8b37ef4f53e5f33899a9994d310
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-06-09 11:01:48 -0400

    agenda tasks card latest

--- src/client/src/features/agenda/TasksCard/index.tsx
+++ src/client/src/features/agenda/TasksCard/index.tsx
@@ -1,19 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { store } from '../../../redux/store'
+import taskActions from '../../../redux/actions/tasks'
 
 
 
 export default function TasksCard() {
+
+    const currentUser = useSelector((state: any) => state.user?.data ?? [])
+    const userTasks = useSelector((state: any) => state.tasks?.queryResult ?? [])
+    const [organizedTasks, setOrganizedTasks] = useState<any>()
+
+    
+    useEffect(() => {
+        store.dispatch(taskActions.setToDos(currentUser?._id))
+    }, [])
+    
+    function isDateAfterWithinDays(date1: any, date2: any, numDays: any) {
+        const timeDifference = date1.getTime() - date2.getTime();
+        const daysInMillis = numDays * 24 * 60 * 60 * 1000;
+        return timeDifference > 0 && timeDifference <= daysInMillis;
+    }
+    
+    useEffect(() => {
+
+        let overdue: any = []
+        let dueWithinSeven: any = []
+        let dueWithinThirty: any = []
+        let dueToday: any = []
+
+        let workingTasks = [...userTasks]
+        workingTasks = workingTasks.filter((tsk: any) => !tsk.isCompleted)
+
+        workingTasks?.forEach((tsk: any) => {
+            const todaysDate = new Date()
+            const taskDueDate = new Date(tsk?.dueDate)
+
+            if (todaysDate > taskDueDate) {
+                overdue.push(tsk)
+
+            } else if (isDateAfterWithinDays(taskDueDate, todaysDate, 7)) {
+                dueWithinSeven.push(tsk)
+
+            }  else if (isDateAfterWithinDays(taskDueDate, todaysDate, 30)) {
+                dueWithinThirty.push(tsk)
+
+            }   else if (isDateAfterWithinDays(taskDueDate, todaysDate, 1)) {
+                dueToday.push(tsk)
+            }
+
+            const sorted = {
+                overdue,
+                dueWithinSeven,
+                dueWithinThirty,
+                dueToday
+            }
+
+            setOrganizedTasks(sorted)
+            
+        })
+
+    }, [userTasks])
+
     return (
-        <div>
-            Tasks Card
+        <div className='p-1'>
             <div>
-                Due Today
+                <h5>Due Today ({`${organizedTasks?.dueToday?.length}`})</h5>
             </div>
             <div>
-                Due Within 7 Days
+                <h5>Due Within 7 Days ({`${organizedTasks?.dueWithinSeven?.length}`})</h5>
             </div>
             <div>
-                Due Within 30 Days
+                <h5>Due Within 30 Days ({`${organizedTasks?.dueWithinThirty?.length}`})</h5>
             </div>
             <div>
                 Upcoming task deadlines render
