492edda101a93dcaa97b4ec8441f6900ddc10b4b
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-06-12 16:46:54 -0400

    Autocomplete for note editor

--- src/client/src/features/notes/NoteEditor/index.tsx
+++ src/client/src/features/notes/NoteEditor/index.tsx
@@ -1,6 +1,9 @@
 import { SaveOutlined } from '@ant-design/icons'
-import { Button, Input, Select } from 'antd'
-import React, { useState } from 'react'
+import { AutoComplete, Button, Input, Select } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { store } from '../../../redux/store'
+import noteActions from '../../../redux/actions/notes'
 
 
 
@@ -12,10 +15,17 @@ interface NoteEditorProps {
 
 export default function NoteEditor(props: NoteEditorProps) {
 
+    const currentUser = useSelector((state: any) => state.user?.data ?? [])
+    const userNotes = useSelector((state: any) => state.notes?.queryResult ?? [])
     const [editorMode, setEditorMode] = useState<any>(props?.mode === 'editing' ? 'edit': 'add')
     const [editingSubject, setEditingSubject] = useState<any>({})
+    const [categoryOptions, setCategoryOptions] = useState<any>([])
 
     const { TextArea } = Input;
+    
+    useEffect(() => {
+        store.dispatch(noteActions.setNotes(currentUser?._id))
+    }, [currentUser])
 
     const onFormChange = (value: any, field: string) => {
         console.log(value, field)
@@ -33,18 +43,45 @@ export default function NoteEditor(props: NoteEditorProps) {
     }
 
     const onSelectCategory = (data: string) => {
-        console.log('data', data)
-        //TODO : Handling for when category is selected
+        console.log('onSelectCategory', data)
+        onFormChange(data, 'category')
     }
 
     const onChangeCategory = (data: string) => {
-        console.log('data', data)
-        //TODO : Handling for when category is changed
+        console.log('onChangeCategory', data)
+        onFormChange(data, 'category')
     }
 
     const onSearchCategory = (data: string) => {
-        console.log('data', data)
-        //TODO : Handling for when category is searched
+        console.log('onSearchCategory', data)
+        onFormChange(data, 'category')
+
+        const options : any= []
+
+        userNotes?.forEach((note: any) => {
+
+            const searchObj = {
+                label: note?.category,
+                value: note?.category,
+            }
+
+            const found = options.some((item: any) => item.value === searchObj.value && item.label === searchObj.label);
+
+            if (!found) {
+                options.push(
+                    {
+                        label: note?.category,
+                        value: note?.category,
+                    }
+                )
+            }
+        })
+        options.push({
+            label: data,
+            value: data,
+        })
+        console.log('options', options)
+        setCategoryOptions(options)
     }
 
 
@@ -58,14 +95,24 @@ export default function NoteEditor(props: NoteEditorProps) {
                         placeholder='Title'
                         size='small'
                         onChange={(e) => onFormChange(e?.target?.value, 'title')}
+                        value={editingSubject?.title}
                     />
                 </div>
                 <div className='w-30 p-1'>
-                    <Select
+                    {/* <Select
                         placeholder='Category'
                         size='small'
                         className='w-100'
                         onChange={(e) => onFormChange(e?.target?.value, 'category')}
+                    /> */}
+                    <AutoComplete
+                        value={editingSubject?.category}
+                        options={categoryOptions}
+                        onSelect={onSelectCategory}
+                        onSearch={onSearchCategory}
+                        onChange={onChangeCategory}
+                        placeholder="control mode"
+                        className='w-100'
                     />
                 </div>
                 <div className='w-10  p-1'>
