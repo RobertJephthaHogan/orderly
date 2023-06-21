1a68fa0ca631b9ffa407d768df65c8541fbe7749
RobertJephthaHogan <robertjephthahogan@gmail.com>
2023-06-20 20:17:32 -0400

    prod url

--- src/client/src/config/Constants.js
+++ src/client/src/config/Constants.js
@@ -1,5 +1,5 @@
-//const API_BASE_URL = 'http://orderlyapi.roberthogan.io'
-const API_BASE_URL = 'http://localhost:8001'
+const API_BASE_URL = 'http://orderlyapi.roberthogan.io'
+//const API_BASE_URL = 'http://localhost:8001'
 
 
 
